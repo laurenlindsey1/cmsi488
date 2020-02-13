b44819320288f89450b3ca4e782a8537f95b034c
Sage Strieker <sspink157@gmail.com>
2020-02-12 17:44:28 -0800

    fixed jest and got the first few tests to pass

--- homework2/regex_practice.js
+++ homework2/regex_practice.js
@@ -1,3 +1,5 @@
+
+
 function isCanadianPostalCode(s){
   return /^[A-Z]\d[A-Z]\s\d[A-Z]\d$/.test(s);
 }
@@ -5,11 +7,14 @@ function isVisa(s) {
   return /^4(\d){15}$/.test(s);
 }
 function isMasterCard(s) {
-  return /^5(\d){15}$/.test(s);
+  return /^5[1-5](\d){14}$/.test(s);
 }
 function isEightThroughTwentyNine(s){
   return /^[8-9]|[12]\d$/.test(s);
 }
 function isMLComment(s){
-  return /^["*"][^"*"]*["*"]$/.test(s);
+  return /^[*][^*]*[*]$/.test(s);
 }
+
+
+module.exports = {isCanadianPostalCode, isVisa, isMasterCard, isEightThroughTwentyNine, isMLComment};
