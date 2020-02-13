60a871f7717a03a0dea62e97bf8d5e75f8bb9b3d
Lauren Lindsey <laurenlinsey19@gmail.com>
2020-02-12 16:51:37 -0800

    add more regex functions

--- homework2/regex_practice.js
+++ homework2/regex_practice.js
@@ -1,3 +1,15 @@
+function isCanadianPostalCode(s){
+  return /^[A-Z]\d[A-Z]\s\d[A-Z]\d$/.test(s);
+}
 function isVisa(s) {
   return /^4(\d){15}$/.test(s);
+}
+function isMasterCard(s) {
+  return /^5(\d){15}$/.test(s);
+}
+function isEightThroughTwentyNine(s){
+  return /^[8-9]|[12]\d$/.test(s);
+}
+function isMLComment(s){
+  return /^["*"][^"*"]*["*"]$/.test(s);
 }
