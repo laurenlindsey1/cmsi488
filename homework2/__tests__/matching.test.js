26a648c5d22cf66937c3a4d19969fab89ef31de7
Sage Strieker <sspink157@gmail.com>
2020-02-23 10:51:00 -0800

    added formatting via prettifier

--- homework2/__tests__/matching.test.js
+++ homework2/__tests__/matching.test.js
@@ -1,57 +1,104 @@
-const assert = require('assert');
-const regexModule = require('../regex_practice/regex_practice');
-const ohmModule = require('../ohm_practice/index');
+const assert = require("assert");
+const regexModule = require("../regex_practice/regex_practice");
+const ohmModule = require("../ohm_practice/index");
 
 const FIXTURE = {
   isCanadianPostalCode: {
-    good: ['A7X 2P8', 'P8E 4R2', 'K1V 9P2', 'Y3J 5C0'],
-    bad: ['A7X   9B2', 'C7E9U2', '', 'Dog', 'K1V\t9P2', ' A7X 2P8', 'A7X 2P8 '],
+    good: ["A7X 2P8", "P8E 4R2", "K1V 9P2", "Y3J 5C0"],
+    bad: ["A7X   9B2", "C7E9U2", "", "Dog", "K1V\t9P2", " A7X 2P8", "A7X 2P8 "]
   },
   isVisa: {
-    good: ['4128976567772613', '4089655522138888', '4098562516243'],
+    good: ["4128976567772613", "4089655522138888", "4098562516243"],
     bad: [
-      '43333', '42346238746283746823', '7687777777263211', 'foo', 'π',
-      '4128976567772613 '
-    ],
+      "43333",
+      "42346238746283746823",
+      "7687777777263211",
+      "foo",
+      "π",
+      "4128976567772613 "
+    ]
   },
   isMasterCard: {
     good: [
-      '5100000000000000', '5294837679998888', '5309888182838282',
-      '5599999999999999', '2221000000000000', '2720999999999999',
-      '2578930481258783', '2230000000000000'
+      "5100000000000000",
+      "5294837679998888",
+      "5309888182838282",
+      "5599999999999999",
+      "2221000000000000",
+      "2720999999999999",
+      "2578930481258783",
+      "2230000000000000"
     ],
     bad: [
-      '5763777373890002', '513988843211541', '51398884321108541', '', 'OH',
-      '5432333xxxxxxxxx'
-    ],
+      "5763777373890002",
+      "513988843211541",
+      "51398884321108541",
+      "",
+      "OH",
+      "5432333xxxxxxxxx"
+    ]
   },
   isAdaFloat: {
     good: [
-      '1', '23_5', '4#20#', '13#fD34_2_1#', '1.3e2', '11_3.3_3_222E-199',
-      '3#1.2#E33', '4e+33'
+      "1",
+      "23_5",
+      "4#20#",
+      "13#fD34_2_1#",
+      "1.3e2",
+      "11_3.3_3_222E-199",
+      "3#1.2#E33",
+      "4e+33"
     ],
-    bad: ['dog', '4fe', 'p#ii#', '_', '_33', '3_', '5__2', '9#88#E-1e3', '-6'],
+    bad: ["dog", "4fe", "p#ii#", "_", "_33", "3_", "5__2", "9#88#E-1e3", "-6"]
   },
   isNotThreeEndingInOO: {
-    good: ['', 'fog', 'Tho', 'one', 'a', 'ab', 'food'],
-    bad: ['fOo', 'gOO', 'HoO', 'zoo', 'MOO', '123', 'A15'],
+    good: ["", "fog", "Tho", "one", "a", "ab", "food"],
+    bad: ["fOo", "gOO", "HoO", "zoo", "MOO", "123", "A15"]
   },
   isDivisibleBy64: {
-    good: ['0', '00', '000', '00000', '00000', '000000', '00000000', '1101000000'],
-    bad: ['1', '00000000100000', '1000000001', 'dog0000000'],
+    good: [
+      "0",
+      "00",
+      "000",
+      "00000",
+      "00000",
+      "000000",
+      "00000000",
+      "1101000000"
+    ],
+    bad: ["1", "00000000100000", "1000000001", "dog0000000"]
   },
   isEightThroughTwentyNine: {
-    good: Array(22).fill(0).map((x, i) => i + 8),
-    bad: ['1', '0', '00003', 'dog', '', '361', '90', '7', '-11'],
+    good: Array(22)
+      .fill(0)
+      .map((x, i) => i + 8),
+    bad: ["1", "0", "00003", "dog", "", "361", "90", "7", "-11"]
   },
   isMLComment: {
-    good: ['(**)', '(*  *)', '(*756****)', '(*****)', '(*(*(******9*)'],
-    bad: ['', '(*)', '(**', 'dog', '(* before (* inner *) after *)', '(* extra space *) '],
+    good: ["(**)", "(*  *)", "(*756****)", "(*****)", "(*(*(******9*)"],
+    bad: [
+      "",
+      "(*)",
+      "(**",
+      "dog",
+      "(* before (* inner *) after *)",
+      "(* extra space *) "
+    ]
   },
   isNotDogDoorDenNoLookAround: {
-    good: ['', 'dogs', 'doors', 'DOG', 'adog', 'zdoor', 'denn', 'deyn', 'dedoor'],
-    bad: ['den', 'door', 'dog'],
-  },
+    good: [
+      "",
+      "dogs",
+      "doors",
+      "DOG",
+      "adog",
+      "zdoor",
+      "denn",
+      "deyn",
+      "dedoor"
+    ],
+    bad: ["den", "door", "dog"]
+  }
 };
 
 // Looks funny, but you can probably figure out what it does
@@ -76,5 +123,5 @@ function runTests(moduleName, module) {
   });
 }
 
-runTests('Regex', regexModule)
-runTests('Ohm', ohmModule)
+runTests("Regex", regexModule);
+runTests("Ohm", ohmModule);
--- homework2/regex_practice/regex_practice.js
+++ homework2/regex_practice/regex_practice.js
@@ -1,29 +1,38 @@
-function isCanadianPostalCode(s){
-  return /^[A-CEGHJ-NPR-TVXY]\d[A-CEGHJ-NPR-TV-Z] \d[A-CEGHJ-NPR-TV-Z]\d$/.test(s);
+function isCanadianPostalCode(s) {
+  return /^[A-CEGHJ-NPR-TVXY]\d[A-CEGHJ-NPR-TV-Z] \d[A-CEGHJ-NPR-TV-Z]\d$/.test(
+    s
+  );
 }
+
 function isVisa(s) {
   return /^4[0-9]{12}(?:[0-9]{3})?$/.test(s);
 }
+
 function isMasterCard(s) {
-  return /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/.test(s);
+  return /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/.test(
+    s
+  );
 }
-function isEightThroughTwentyNine(s){
+
+function isEightThroughTwentyNine(s) {
   return /^([8-9]|[12]\d)$/.test(s);
 }
-function isMLComment(s){
+
+function isMLComment(s) {
   return /^\(\*(\*(?!\))|[^\*])*\*\)$/.test(s);
 }
 
-function isNotThreeEndingInOO(s){
+function isNotThreeEndingInOO(s) {
   return /^(^(?![A-Z]oo$)[A-Z]*)$/i.test(s);
 }
 
-function isDivisibleBy64(s){
+function isDivisibleBy64(s) {
   return /^((0+)|([01]*10{6}))$/.test(s);
 }
 
-function isNotDOgDoorDenNoLookAround(s){
- return new RegExp(`^(
+function isNotDogDoorDenNoLookAround(s) {
+  return new RegExp(
+    `^(
     [A-Za-ce-z]([A-Za-z]*)|
     d([A-Za-df-np-z]([A-Za-z]*))?|
     de([A-Za-mo-z]([A-Za-z]*))?|
@@ -32,17 +41,29 @@ function isNotDOgDoorDenNoLookAround(s){
     dog([A-Za-z]+)|
     doo([A-Za-qs-z]([A-Za-z]*))?|
     door([A-Za-z]+)
-  )?$`.replace(/\s+/g, '')).test(s);
+  )?$`.replace(/\s+/g, "")
+  ).test(s);
 }
 
-function isNotDOgDoorDenWithLookAround(s) {
+function isNotDogDoorDenWithLookAround(s) {
   return /^(?!(dog$|den$|door$))[A-Za-z]*$/.test(s);
 }
 
-function isAdaFloat(s){
-  return /^((\d+(_\d+)*#((\d|[a-fA-F])+(_?(\d|[a-fA-F]))*(\.(\d|[a-fA-F])(_(\d|[a-fA-F]))*)?#([eE]\+?(\d+(_\d+)*)|[eE]-(\d+(_\d+)*))?))|((\d+(_\d+)*(\.\d+(_\d+)*)?)([eE]\+?(\d+(_\d+)*)|[eE]-(\d+(_\d+)*))?))$/.test(s);
+function isAdaFloat(s) {
+  return /^((\d+(_\d+)*#((\d|[a-fA-F])+(_?(\d|[a-fA-F]))*(\.(\d|[a-fA-F])(_(\d|[a-fA-F]))*)?#([eE]\+?(\d+(_\d+)*)|[eE]-(\d+(_\d+)*))?))|((\d+(_\d+)*(\.\d+(_\d+)*)?)([eE]\+?(\d+(_\d+)*)|[eE]-(\d+(_\d+)*))?))$/.test(
+    s
+  );
 }
 
-
-
-module.exports = {isCanadianPostalCode, isVisa, isMasterCard, isEightThroughTwentyNine, isMLComment, isNotThreeEndingInOO, isDivisibleBy64, isAdaFloat, isNotDOgDoorDenNoLookAround, isNotDOgDoorDenWithLookAround};
+module.exports = {
+  isCanadianPostalCode,
+  isVisa,
+  isMasterCard,
+  isEightThroughTwentyNine,
+  isMLComment,
+  isNotThreeEndingInOO,
+  isDivisibleBy64,
+  isAdaFloat,
+  isNotDogDoorDenNoLookAround,
+  isNotDogDoorDenWithLookAround
+};
